a6baa9b43570777d9c0e29e28b060ace153e610a
mirkoscotti2005@gmail.com <mirkoscotti2005@gmail.com>
2024-03-23 19:42:25 +0000

    prova

--- login.php
+++ login.php
@@ -8,6 +8,16 @@
   </head>
   <body>
     <h1>Esegui il login</h1>
+    <form action="login.php" method="post">
+      <div class="mb-3">
+        <label for="email" class="form-label">email</label>
+        <input type="email" class="form-control" id="email" name="email">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
